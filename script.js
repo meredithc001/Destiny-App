302dbc4782356fed93b2b375dc6b1ae8f5fc88cf
Carson Meredith <meredithc@msoe.edu>
2023-01-09 16:04:03 -0600

    Get Request works in browser now, will hopefully work with API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var myHeaders = new Headers();
 myHeaders.append("x-api-key", "e9850496e3fa470b9f1ca8627b8303c4");
 myHeaders.append("Authorization", "Basic NDI1NzI6U09Nd2NzckV0LjFQNkdFWEJJVFVtZTA5YUV0dGFXRFd3SU9ZQVlqVC12NA==");
-myHeaders.append("Cookie", "Q6dA7j3mn3WPBQVV6Vru5CbQXv0q+I9ddZfGro+PognXQwjWM8bM6VGC=v1UdlRgw__XO+; __cflb=0H28vP5GxS7vgVH4MZT6rB7QcDNQ8jpmHaN1cfyE7As; bungleanon=sv=BAAAAACtTgAAAAAAAEveEwQAAAAAAAAAAAAAAADEnxFYVPDaCEAAAACKqgDt6YbukUhkkteevSz8bvYnXHIL492M8oHsZjVtDEZyjtv4azM/4D5JQX2KqifUhCKepyATm2/C08cpjIfb&cl=MC4yMDE0MS42ODQxMDk1NQ==; bungled=6547675847753298509; bungledid=B9jraAzZr0lLvA0MOFoHU96zeBFYVPDaCAAA");
+//myHeaders.append("Cookie", "__cflb=0H28vP5GxS7vgVH4MZT6rB7QcDNQ8jpmFtu3BPdYQaF; bungleanon=sv=BAAAAACuTgAAAAAAAKuHcQQAAAAAAAAAAAAAAADd6WMc4vHaCEAAAAD63p7kMx2KZmz25pQvWZDjNf+CHPfbCFDS4LsH0vKSArDN4+8BNp9iedoQKq4XYqyE42If05tbsJksVlskDWSr&cl=MC4yMDE0Mi43NDU0OTE2Mw==; bungled=1898624053759548391; bungledid=B6DIMu6YyLJJgbAqqC3mFh3HwmMc4vHaCAAA");
 
 var requestOptions = {
     method: 'GET',
@@ -9,7 +9,7 @@ var requestOptions = {
     redirect: 'follow'
 };
 
-fetch("https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018451018706/?components=200", requestOptions)
+fetch("https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018451018706/?components=205", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
